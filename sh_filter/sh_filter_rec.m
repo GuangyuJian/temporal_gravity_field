4a3b3d2b5a29608b95d54f2cb8b29296baad57da
GuangyuJian <133509039+GuangyuJian@users.noreply.github.com>
2023-12-10 01:41:51 +0800

    Add files via upload

---
+++ sh_filter/sh_filter_rec.m
@@ -0,0 +1,23 @@
+function [wnm]=sh_filter_rec(wnm_basic,recn)                               
+%                              
+%**************************************************************************
+% In   : 
+%       wnm_basic     [(Nmax+2)*(Nmax+1)/2 x 1]   
+%                      basic weighted coefficients storaged as clm-format   
+%       recn          [1x1] reconstructive number (int)
+% Out  :    
+%       wnm           [(Nmax+2)*(Nmax+1)/2 x 1]   
+%                     reconstructive weighted coefficients storaged as clm-format
+%----------------------------------------------------------------------------
+% Authors: Karl Jian
+% address: Guangdong University of Technology(GDUT)
+% email: gyjian@mail2.gdut.edu.cn
+% date: 2023-12-10
+%**************************************************************************
+% Ref: Jian, Guangyu, Chuang Xu, Fang Zou, and Bo Huang. 2023. 
+% ‘A Novel GRACE Reconstructive Filter to Extract the Mass Changes in Madagascar’. 
+% GEOPHYSICAL JOURNAL INTERNATIONAL 235(2): 1493–1503.
+%**************************************************************************
+%
+    wnm=1-(1-wnm_basic).^recn;
+end
